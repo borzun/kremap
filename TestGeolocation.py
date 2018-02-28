940538448c4660ac23d317f64908a0e3faba8f69
Borzun <bohdan.kurylovych@gmail.com>
2018-03-01 00:49:26 +0200

    MINOR: added small changes to TestGeolocation

--- TestGeolocation.py
+++ TestGeolocation.py
@@ -5,21 +5,17 @@ import WebGoogleMaps
 
 
 def run_tests(gmaps_client, location):
+    print("<------ GeoCode_{}_STARTED ------> ".format(location))
     # Geocoding an address
     geocode_result = gmaps_client.geocode(location)
-    pprint.pprint(geocode_result, depth=3)
+    if geocode_result is None:
+        print("     ----- GeoCode_{}_FAILED: geocode_result is None ----->".format(location))
+    else:
+        print("     ----- GeoCode_{}_SUCCESS ----->".format(location))
 
     geocode = GeoCode.Parser(geocode_result)
     status = geocode.get_status()
     place_id = geocode.get_place_id()
-    print("------ GeoCode test: status:{0}; place_id:{1} ------ ".format(status, place_id))
+    print("     ------ GeoCode_{0}_Data: status:{1}; place_id:{2} ------ ".format(location, status, place_id))
 
-    direction_output = gmaps_client.directions('Житловий комплекс "Славутич"', 'Paris', mode='driving', units='metric')
-    pprint.pprint(direction_output, depth=7)
-
-    direction_route = DirectionRoute.RouteParser(direction_output)
-    status = direction_route.get_status()
-    duration = direction_route.get_duration()
-    print("------ DirectionRoute test: status:{0}; duration:{1} ------ ".format(status, duration))
-
-    'WebGoogleMaps.open_with_place_id(place_id)'
+    WebGoogleMaps.open_with_place_id(place_id)
